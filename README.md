1f089aa315aa76fefd80b806e948057c558b11dd
Robert Sherman <122405581+rsherman1@users.noreply.github.com>
2023-05-10 13:43:33 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Quote Carousel
+
+[![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/ewuweblab/quote-carousel)
+
+A carousel of quotes.
+
+## Why does this exist? 
+- Understand pros and cons of the UI pattern: sliders or carousels
+- Create solution without using depending on a third-party JS library
+- Web experience that can be navigated without a mouse and keyboard
+
+## Citations
+- Your citations and sources go here
---
+++ js/script.js
@@ -0,0 +1,159 @@
+/* Data to Show*/
+const data = [
+  {
+   quote: `“Be like a mountain.”`, 
+   emoji: '🗻', 
+   image: `url(assets/mountain.jpg)`
+  }, 
+  {
+   quote: `“Brave the oceans.”`, 
+   emoji: '🌊', 
+   image: `url(assets/ocean.jpg)`
+  },  
+  {
+   quote: `“Grow like a tree.”`, 
+   emoji: '🌲' , 
+   image: `url(assets/tree.jpg)`
+  },
+  {
+    quote: `“We'll be ok now.”`, 
+    emoji: '👌', 
+    image: `url(assets/forest.jpg)`
+  }
+];
+
+/*
+Quote and Emoji
+*/
+// blockquote
+const quote = document.querySelector('blockquote');
+// emoji
+const emoji = document.querySelector('p');
+// background
+const bgImage = document.querySelector('body');
+
+// Check-Check: change text
+quote.innerHTML = "Colin is quotable.";
+// Check-Check: change emoji text
+emoji.innerHTML = "👍️";
+// Check-Check: background image
+bgImage.style.backgroundImage= 'url(/assets/mountain.jpg)';
+
+/*
+ * Navigate to Items with Event Listeners
+ * Track Number of Items
+*/
+// initialize item number
+let itemNumber = 0;
+// get next button
+const nextButton = document.querySelector("img.next");
+// get previous button
+const prevButton = document.querySelector("img.prev");
+// get random button
+const randButton = document.querySelector("img.random");
+
+// listen for clicks on next button
+nextButton.addEventListener("click", () => {
+  // update, then increase item number with each click
+  itemNumber = itemNumber + 1;
+  
+  // if at LAST item
+  if ( itemNumber > data.length -1 ) {
+    // reset counting to first item
+    itemNumber = 0;
+  }
+
+  // display data based on array index
+  quote.innerHTML = data[itemNumber].quote;
+  emoji.innerHTML = data[itemNumber].emoji;
+  bgImage.style.backgroundImage = data[itemNumber].image;
+  
+});
+  
+// listen for clicks on previous button
+prevButton.addEventListener("click", () => {
+  // update, then DEcrease item number with each click
+  itemNumber = itemNumber - 1;
+  
+  // if at FIRST item
+  if ( itemNumber < 0 ) {
+    // reset counting to last item
+    itemNumber = data.length -1;
+  }  
+  
+  // display data based on array index
+  quote.innerHTML = data[itemNumber].quote;
+  emoji.innerHTML = data[itemNumber].emoji;
+  bgImage.style.backgroundImage = data[itemNumber].image;
+});
+
+// listen for clicks on random button
+randButton.addEventListener('click', () => {
+  
+  // generate random number, for total number, of items (data)
+  randomNumber = Math.floor ( Math.random() * data.length );
+  
+  // display data based random number
+  quote.innerHTML = data[randomNumber].quote;
+  emoji.innerHTML = data[randomNumber].emoji;
+  bgImage.style.backgroundImage = data[randomNumber].image;
+  
+});
+
+// listen for keypress next
+document.addEventListener('keyup', (event) => {
+  
+  // check-check: What key was pressed?
+  // console.log(event);
+
+  // Next Right Arrow  
+  if ( event.key === "ArrowRight" ) {
+   
+   // update, then increase item number with each click
+  itemNumber = itemNumber + 1;
+  
+  // if at LAST item
+  if ( itemNumber > data.length -1 ) {
+    // reset counting to first item
+    itemNumber = 0;
+  }
+
+  // display data based on array index
+  quote.innerHTML = data[itemNumber].quote;
+  emoji.innerHTML = data[itemNumber].emoji;
+  bgImage.style.backgroundImage = data[itemNumber].image;
+    
+  }
+  
+  // Prev Left Arrow
+  if ( event.key === "ArrowLeft" ) {
+  
+   // update, then DEcrease item number with each click
+  itemNumber = itemNumber - 1;
+  
+  // if at FIRST item
+  if ( itemNumber < 0 ) {
+    // reset counting to last item
+    itemNumber = data.length -1;
+  }  
+  
+  // display data based on array index
+  quote.innerHTML = data[itemNumber].quote;
+  emoji.innerHTML = data[itemNumber].emoji;
+  bgImage.style.backgroundImage = data[itemNumber].image;
+  }
+  
+  // Random Spacebar
+  if ( event.key === " ") {
+    
+    // generate random number, for total number, of items (data)
+    randomNumber = Math.floor ( Math.random() * data.length );
+
+    // display data based random number
+    quote.innerHTML = data[randomNumber].quote;
+    emoji.innerHTML = data[randomNumber].emoji;
+    bgImage.style.backgroundImage = data[randomNumber].image;
+  
+  }
+  
+});
